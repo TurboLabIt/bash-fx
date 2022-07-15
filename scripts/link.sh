0c01d1826eeb906043b0de4f362b50fdd8c5f510
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-07-15 14:03:43 +0200

    Create link.sh

---
+++ scripts/link.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+function fxLinkBin()
+{
+  EXECUTABLE=$1
+  LINK_NAME=$2
+  
+  if [ -z "$LINK_NAME" ]; then
+    LINK_NAME=$(basename "$EXECUTABLE" .sh)
+  fi
+  
+  if [ -z "$EXECUTABLE" ] || [ -z "$LINK_NAME" ]; then
+    fxCatastrophicError "Usage: pathOfTheScript linkName"
+  fi
+  
+  if [ ! -f "$EXECUTABLE" ]; then
+    fxCatastrophicError "$1 doesn't exist!"
+  fi
+  
+  fxTitle "🧹 Removing existing $2 links..."
+  rm -f "/usr/bin/$2" "/usr/local/bin/$2"
+  
+  fxTitle "🔗 Linking..."
+  fxMessage "Script: ${EXECUTABLE}"
+  fxMessage "Script: ${LINK_NAME}"
+  ln -s "$1" "/usr/local/bin/$2"
+}
