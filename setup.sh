2b2edefa9cecd290772e4991b84c468a995d65b3
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-08-28 17:28:40 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 SCRIPT_NAME=bash-fx
 
-sudo apt update && sudo apt install curl -y
+if [ -z $(command -v curl) ]; then sudo apt update && sudo apt install curl -y; fi
 
 ## begin
 curl -o /tmp/bash-fx-setup-begin.sh -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/main/setup/start.sh
