c0f67deea159100426ba7cdd2d8336164834a638
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-08-30 09:57:10 +0200

    Update link.sh

--- scripts/link.sh
+++ scripts/link.sh
@@ -23,3 +23,23 @@ function fxLinkBin()
   echo "🔗 Link:   ${LINK_NAME}"
   sudo ln -s "$EXECUTABLE" "/usr/local/bin/$LINK_NAME"
 }
+
+
+function fxCopyCron()
+{
+  local ORIGIN_CRON_FILEPATH=$1
+  
+  if [ -z "$2" ]; then
+    local DEST_CRON_FILE_PATH="/etc/cron.d/$(basename -- ${ORIGIN_CRON_FILEPATH})"
+  else
+    local DEST_CRON_FILE_PATH="/etc/cron.d/$2"
+  fi
+  
+  fxTitle "🕛 Copying cron file..."
+  echo "💻 Origin: ${ORIGIN_CRON_FILEPATH}"
+  echo "🕕 Destin: ${DEST_CRON_FILE_PATH}"
+  
+  sudo cp "${ORIGIN_CRON_FILEPATH}" "${DEST_CRON_FILE_PATH}"
+  sudo chown root:root "${DEST_CRON_FILE_PATH}"
+  sudo chmod u=rw,go= "${DEST_CRON_FILE_PATH}"
+}
