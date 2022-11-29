1273f35fa5920cc64794db24f36af47f21d94810
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-11-29 20:56:05 +0100

    Update network.sh

--- scripts/network.sh
+++ scripts/network.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
 
+function fxReadRemoteFile()
+{
+  local URL_TO_READ=$1 
+  if [ -z "$URL_TO_READ" ]; then
+    fxCatastrophicError "fxReadRemoteFile usage: https://turbolab.it/scarica/204"
+  fi
+  
+  local FILE_CONTENT=$(curl -L "$URL_TO_READ")
+  echo "$FILE_CONTENT"
+}
