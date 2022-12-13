ef28e6115f583dac94fbdd0312d1610267ea4502
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-12-13 21:28:21 +0100

    Create replace.sh

---
+++ scripts/replace.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+function fxReplaceContentInDirectory()
+{
+  if [ ! -d "$1" ]; then
+    fxCatastrophicError "fxReplaceContentInDirectory: ##$1## is not a directory!"
+  fi
+
+  if [ ! -z "$2" ]; then
+    fxCatastrophicError "fxReplaceContentInDirectory:content to replace ##$2## is undefined!"
+  fi
+
+  find "$1" \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's|$2|$3|g'
+}
