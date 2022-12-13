002a274cf41df69fe8371049d3f13023d430a954
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-12-13 22:43:55 +0100

    Update replace.sh

--- scripts/replace.sh
+++ scripts/replace.sh
@@ -6,8 +6,8 @@ function fxReplaceContentInDirectory()
     fxCatastrophicError "fxReplaceContentInDirectory: ##$1## is not a directory!"
   fi
 
-  if [ ! -z "$2" ]; then
-    fxCatastrophicError "fxReplaceContentInDirectory:content to replace ##$2## is undefined!"
+  if [ -z "$2" ]; then
+    fxCatastrophicError "fxReplaceContentInDirectory: content to replace ##$2## is undefined!"
   fi
 
   find "$1" \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's|$2|$3|g'
