a7b16bb3504b254dac1ea48a982555d0e637bb2a
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-12-23 10:17:04 +0100

    Update app-env.sh

--- scripts/app-env.sh
+++ scripts/app-env.sh
@@ -1,24 +1,32 @@
 #!/usr/bin/env bash
 
+if [ ! -z "${PROJECT_DIR}" ]; then
+  PROJECT_DIR_OWNER=$(fxGetFileOwner ${PROJECT_DIR})
+fi
+
+
 if [ ! -z "${PROJECT_DIR}" ] && [ -f "${PROJECT_DIR}env" ]; then
 
   APP_ENV=$(head -n 1 ${PROJECT_DIR}env)
 
-elif [ ! -z "${PROJECT_DIR}" ] && [ -d "${PROJECT_DIR}.git" ]; then
+elif [ ! -z "${PROJECT_DIR}" ] && [ -d "${PROJECT_DIR}.git" ] && [ "$(whoami)" = "$PROJECT_DIR_OWNER" ]; then
 
-  #git config --global --add safe.directory "${PROJECT_DIR}"
   GIT_BRANCH=$(git -C $PROJECT_DIR branch | grep \* | cut -d ' ' -f2-)
+
+elif [ ! -z "${PROJECT_DIR}" ] && [ -d "${PROJECT_DIR}.git" ] && [ "$(whoami)" != "$PROJECT_DIR_OWNER" ]; then
+
+  GIT_BRANCH=$(sudo -u $PROJECT_DIR_OWNER -H git -C $PROJECT_DIR branch | grep \* | cut -d ' ' -f2-)
 fi
 
 
 if [ "$GIT_BRANCH" = "master" ]; then
 
   APP_ENV=prod
-
+  
 elif [ "$GIT_BRANCH" = "staging" ]; then
 
   APP_ENV=staging
-
+  
 elif [ "$GIT_BRANCH" = "dev" ] || [[ "$GIT_BRANCH" = "dev-"* ]]; then
 
   APP_ENV=dev
