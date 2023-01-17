c1f545588b772df8a73d95a49fee758522673ecc
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-17 22:17:17 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,6 @@ SCRIPT_NAME=bash-fx
 
 if [ -z $(command -v curl) ]; then sudo apt update && sudo apt install curl -y; fi
 
-## suppress needrestart
-sudo curl -Lo /etc/needrestart/conf.d/zzupdate-needrestart-suppress.conf https://raw.githubusercontent.com/TurboLabIt/zzupdate/master/needrestart-suppress.conf?$(date +%s)
-
 ## begin
 curl -o /tmp/bash-fx-setup-begin.sh -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/main/setup/start.sh
 sudo bash /tmp/bash-fx-setup-begin.sh ${SCRIPT_NAME}
