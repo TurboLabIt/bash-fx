aac093aeda28174d8ce5b33a53855d18806a176c
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-19 21:58:39 +0100

    Update git.sh

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,52 +1,58 @@
 function fxGitCheckForUpdate()
 {
   if [ -z "$1" ]; then
-    local PATH=$(pwd)
+    local PROJECT_DIR=$(pwd)
   else
-    local PATH=$1
+    local PROJECT_DIR=$1
   fi
-  
+
   local SILENT_MODE=$2
-  
+
   if [ -z "$SILENT_MODE" ]; then
    fxTitle "🔎 Checking if a new git revision is available..."
   fi
-  
+
   local UPSTREAM=${1:-'@{u}'}
-  local LOCAL=$(git -C "${PATH}" rev-parse @)
-  local REMOTE=$(git -C "${PATH}" rev-parse "$UPSTREAM")
-  local BASE=$(git -C "${PATH}" merge-base @ "$UPSTREAM")
+  local LOCAL=$(git -C "${PROJECT_DIR}" rev-parse @)
+  local REMOTE=$(git -C "${PROJECT_DIR}" rev-parse "$UPSTREAM")
+  local BASE=$(git -C "${PROJECT_DIR}" merge-base @ "$UPSTREAM")
 
   if [ $LOCAL = $REMOTE ]; then
-  
+
     if [ -z "$SILENT_MODE" ]; then
       fxOK "Up-to-date"
     fi
-  
+
     return ""
-    
-  elif [ $LOCAL = $BASE ]; then
-  
+
+  fi
+
+
+  if [ $LOCAL = $BASE ]; then
+
     if [ -z "$SILENT_MODE" ]; then
       fxWarning "Need to pull"
     fi
-    
+
     return 1
-    
-  elif [ $REMOTE = $BASE ]; then
-  
+
+  fi
+
+
+  if [ $REMOTE = $BASE ]; then
+
     if [ -z "$SILENT_MODE" ]; then
       fxWarning "Need to push"
     fi
-  
+
     return 2
-    
-  else
-  
-     if [ -z "$SILENT_MODE" ]; then
-      fxWarning "Branch diverged"
-    fi
-    
-    return 3
+
   fi
+
+
+  if [ -z "$SILENT_MODE" ]; then
+      fxWarning "Branch diverged"
+  fi
+
+  return 3
 }
