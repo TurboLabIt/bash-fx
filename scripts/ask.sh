273625363b5239e73a6fadaf8e91468e3bebb936
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2025-05-03 14:47:32 +0200

    Update ask.sh

--- scripts/ask.sh
+++ scripts/ask.sh
@@ -1,1 +1,14 @@
+function fxAskConfirmation()
+{
+  if [ ! -z "$1" ]; then
+    local CONFIRM_MESSAGE=$1
+  else
+    local CONFIRM_MESSAGE="Proceed? [Y/N]"
+  fi
 
+  fxWarning "$CONFIRM_MESSAGE"
+  read -p ">> " -n 1 -r  < /dev/tty
+  if [[ ! "$REPLY" =~ ^[Yy1]$ ]]; then
+    fxCatastrophicError "Aborted by user"
+  fi
+}
