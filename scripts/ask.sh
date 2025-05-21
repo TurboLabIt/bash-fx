48061a482933a65003ed0d88c60e2a0cb4eaf3ef
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2025-05-21 07:50:49 +0200

    Update ask.sh

--- scripts/ask.sh
+++ scripts/ask.sh
@@ -7,8 +7,19 @@ function fxAskConfirmation()
   fi
 
   fxWarning "$CONFIRM_MESSAGE"
-  read -p ">> " -n 1 -r  < /dev/tty
-  if [[ ! "$REPLY" =~ ^[Yy1]$ ]]; then
-    fxCatastrophicError "Aborted by user"
+
+  if [ -t 0 ]; then
+
+    # Running in a terminal, interactively ask for confirmation
+    read -p ">> " -n 1 -r < /dev/tty
+    echo
+    if [[ ! "$REPLY" =~ ^[Yy1]$ ]]; then
+      fxCatastrophicError "Aborted by user"
+    fi
+    
+  else
+  
+    # Not running in a terminal (e.g., via cron), proceed automatically
+    fxWarning "No terminal detected (non-interactive environment). Proceeding automatically."
   fi
 }
